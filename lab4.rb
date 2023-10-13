02e3bf26a9cb75ca520a9ca4266d254cfeec8665
root <root@SC-202005091355>
2023-10-13 13:12:21 +0800

    更正

--- lab4.rb
+++ lab4.rb
@@ -1,6 +1,10 @@
 def decode_ways(code)
-    aa=code.chomp.chars
-    dd=Array.new(aa.size,1)
-    aa.each_with_index do |char,index| dd[index]=(char=="0"?0:1)*dd[index-1]+(( index!=0 && ((aa[index-1]=="1")||(("0".."6")===aa[index-1]&&aa[index-1]=="2")))?1:0)*(index!=0?dd[index-2]:0)end
-    return dd[aa.size-1]
+  aa = code.to_s.chars
+  dd = Array.new(aa.size, 1)
+  aa.each_with_index do |char, index|
+    dd[index] =
+      (char == '0' ? 0 : 1) * dd[index - 1] + (index >=1 && ((aa[index - 1] == '1') || (('0'..'6').include?(char) && index >= 1 && aa[index - 1] == '2')) ? 1 : 0) * (index >= 1 ? dd[index - 2] : 0)
+  end
+  dd[aa.size - 1]
 end
+#puts decode_ways(1271026)
